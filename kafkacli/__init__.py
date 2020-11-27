a12b24a903c231252552145b0b1b3eea432d3e8e
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-11-27 14:36:03 +0700

    update kafka cli module

--- kafkacli/__init__.py
+++ kafkacli/__init__.py
@@ -2,4 +2,12 @@ from kafkacli.args_parser import (
     ArgsParser, __version__
 )
 
+from kafkacli.killer import (
+    Killer,
+)
+
+from kafkacli.k import (
+    K,
+)
+
 VERSION = tuple([int(v) for v in __version__.split('.')])
